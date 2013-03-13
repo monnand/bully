293d5042255de7a82a6dd2cc52b0e2642e5bc3be
monnand <monnand@gmail.com>
2013-03-12 22:34:55 -0400

    [Add] Command test cases

---
+++ command_test.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"testing"
+	"bytes"
+)
+
+func cmdEq(a, b *command) bool {
+	if a.Cmd != b.Cmd {
+		return false
+	}
+	if len(a.Header) != len(b.Header) {
+		return false
+	}
+	for k, v := range a.Header {
+		if b.Header[k] != v {
+			return false
+		}
+	}
+	for i, x := range a.Body {
+		if b.Body[i] != x {
+			return false
+		}
+	}
+	return true
+}
+
+func testWriteThenRead(cmd *command, t *testing.T) {
+	buf := make([]byte, 0, 2048)
+	byteBuffer := bytes.NewBuffer(buf)
+	err := writeCommand(byteBuffer, cmd)
+	if err != nil {
+		t.Errorf("Write error: %v\n", err)
+	}
+
+	shadow, err := readCommand(byteBuffer)
+	if err != nil {
+		t.Errorf("Read error: %v\n", err)
+	}
+
+	if !cmdEq(shadow, cmd) {
+		t.Errorf("Not equal")
+	}
+}
+
+func TestFullCommand(t *testing.T) {
+	cmd := new(command)
+	cmd.Cmd = uint8(1)
+	cmd.Header = make(map[string]string, 10)
+	cmd.Header["h1"] = "header 1"
+	cmd.Header["h2"] = "header 2"
+	cmd.Body = []byte{1,2,54,2}
+	testWriteThenRead(cmd, t)
+}
+
+func TestCommandWithBodyOnly(t *testing.T) {
+	cmd := new(command)
+	cmd.Cmd = uint8(1)
+	cmd.Header = make(map[string]string, 10)
+	cmd.Body = []byte{1,2,54,2}
+	testWriteThenRead(cmd, t)
+}
+
+func TestCommandWithHeaderOnly(t *testing.T) {
+	cmd := new(command)
+	cmd.Cmd = uint8(1)
+	cmd.Header = make(map[string]string, 10)
+	cmd.Header["h1"] = "header 1"
+	cmd.Header["h2"] = "header 2"
+	testWriteThenRead(cmd, t)
+}
+
