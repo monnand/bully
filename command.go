50a4c8ac2461367d656dd9d262c15050a8574706
monnand <monnand@gmail.com>
2013-03-15 17:33:00 -0400

    [Add] Copyright Notice

--- command.go
+++ command.go
@@ -1,3 +1,20 @@
+/*
+ * Copyright 2012 Nan Deng
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 package main
 
 import (
@@ -18,8 +35,8 @@ const (
 	cmdELECT       uint8 = 6
 	cmdELECT_OK    uint8 = 7
 	cmdCOORDIN     uint8 = 8
-	cmdTRY_LATER uint8 = 9
-	cmdDUP_EXIT uint8 = 10
+	cmdTRY_LATER   uint8 = 9
+	cmdDUP_EXIT    uint8 = 10
 )
 
 type command struct {
