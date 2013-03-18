73775a76b4a66c3c6673be999c7e8caa73ce33da
monnand <monnand@gmail.com>
2013-03-18 04:52:43 -0400

    [Mod] tested 5-bully case. No error on my machine. need to remove debug messages

--- bully_test.go
+++ bully_test.go
@@ -181,3 +181,17 @@ func Test4BullyElect(t *testing.T) {
 	fmt.Println("-------------------#------------------")
 }
 
+func Test5BullyElect(t *testing.T) {
+	fmt.Println("-------------------5------------------")
+	bullies := buildBullies(8088, 5, t)
+	for _, b := range bullies {
+		fmt.Printf("bully: %v on addr %v\n", b.MyId(), b.MyAddr())
+	}
+	buildConnections(bullies, t)
+	testSameViewOnBullies(bullies, t)
+	testSameLeader(bullies, t)
+	fmt.Println("-------------------*------------------")
+	cleanBullies(bullies)
+	fmt.Println("-------------------#------------------")
+}
+
